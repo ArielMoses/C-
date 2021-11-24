d3d7f0f51afaa1a09a13c34a8a2cb6ff4308b5a7
Ariel Moses <92585669+ArielMoses@users.noreply.github.com>
2021-11-24 08:37:13 +0200

    Update 9.12.21.cpp

--- 9.12.21.cpp
+++ 9.12.21.cpp
@@ -33,6 +33,45 @@ int main() {
 		for (int b = a - 1; b > 1; b--) {
 			if (a % b != 0) {
 				flag2 = true;
+				
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	#include <iostream>
+
+using namespace std;
+
+
+int sub(int num1, int num2) {
+	int i = 0;
+	if (num1 > num2) {
+		i = num2;
+	}
+	if (num1 < num2) {
+		i = num1;
+	}
+	else {
+		i = num1;
+	}
+	for (i; i > 1; i--) {
+		if (num1 % i == 0 && num2 % i == 0) {
+			cout << i;
+			break;
+		}
+	}
+	return 0;
+}
+
+
+int main() {
+	int num1;
+	int num2;
+	cout << "Enter the first Number:";
+	cin >> num1;
+	cout << "Enter the second Number:";
+	cin >> num2;
+	sub(num1, num2);
+
+}
+
 			}
 			else {
 				flag2 = false;
